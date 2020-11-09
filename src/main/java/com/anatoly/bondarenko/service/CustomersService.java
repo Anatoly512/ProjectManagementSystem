019888fcfc013f3d3003e17d11aa6a6b4ffbbaaa
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2020-11-09 23:28:01 +0200

    add customersService

--- src/main/java/com/anatoly/bondarenko/service/CustomersService.java
+++ src/main/java/com/anatoly/bondarenko/service/CustomersService.java
@@ -1,7 +1,43 @@
 package com.anatoly.bondarenko.service;
 
+import com.anatoly.bondarenko.DAO.CustomersDAO;
+import com.anatoly.bondarenko.domain.Customers;
+
+import java.util.List;
+
+
 public class CustomersService {
 
+    private final CustomersDAO customersDAO;
+
+
+    public CustomersService(CustomersDAO customersDAO) {
+        this.customersDAO = customersDAO;
+    }
+
+
+    public List<Customers> getAllCustomers() {
+
+        return customersDAO.getAllEntities();
+    }
+
+
+    public void deleteCustomer(Long id) {
+
+        customersDAO.deleteEntity(id);
+    }
+
+
+    public void addNewCustomer(Customers customer) {
+
+        customersDAO.addNewEntity(customer);
+    }
+
+
+    public void updateCustomer(Long id, Customers customer) {
+
+        customersDAO.updateEntity(id, customer);
+    }
 
 
 }
