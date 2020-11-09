da48e79a0f035db4324e18afa4917311276d1b7c
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2020-11-09 23:02:19 +0200

    domains add

--- src/main/java/com/anatoly/bondarenko/domain/Projects.java
+++ src/main/java/com/anatoly/bondarenko/domain/Projects.java
@@ -1,4 +1,55 @@
 package com.anatoly.bondarenko.domain;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class Projects {
+
+    private Long id;
+    private String projectsName;
+    private BigDecimal cost;
+
+
+    public Projects(Long id, String projectsName, BigDecimal cost) {
+        this.id = id;
+        this.projectsName = projectsName;
+        this.cost = cost;
+    }
+
+    public Projects() {     //  Этот конструктор нужен для корректной работы класса DevelopersDAO,
+                            //  пустые поля потом (после создания объекта) заполняются через сеттеры
+    }
+
+
+// getters  (Lombok почему-то не создал их автоматически, поэтому по старинке)
+
+    public String getProjectsName() {
+        return this.projectsName;
+    }
+
+    public BigDecimal getCost() {
+        return this.cost;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+
+//  setters
+
+    public void setProjectsName(String projectsName) {
+        this.projectsName = projectsName;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
--- src/main/java/com/anatoly/bondarenko/domain/Skills.java
+++ src/main/java/com/anatoly/bondarenko/domain/Skills.java
@@ -1,4 +1,53 @@
 package com.anatoly.bondarenko.domain;
 
+import lombok.Data;
+
+@Data
 public class Skills {
+
+    private Long id;
+    private String language;
+    private SkillLevel skillLevel;
+
+    public Skills(Long id, String language, SkillLevel skillLevel) {
+        this.id = id;
+        this.language = language;
+        this.skillLevel = skillLevel;
+    }
+
+    public Skills() {     //  Этот конструктор нужен для корректной работы класса DevelopersDAO,
+                          //  пустые поля потом (после создания объекта) заполняются через сеттеры
+    }
+
+
+// getters  (Lombok почему-то не создал их автоматически, поэтому по старинке)
+
+    public String getLanguage() {
+        return this.language;
+    }
+
+    public SkillLevel getSkillLevel() {
+        return this.skillLevel;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+
+//  setters
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setSkillLevel(SkillLevel skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
 }
