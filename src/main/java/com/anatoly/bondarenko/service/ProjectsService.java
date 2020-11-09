639f4ca52d18fd6b5ee2c60570665fd99e71c592
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2020-11-10 00:21:58 +0200

    add projects and skills logic

--- src/main/java/com/anatoly/bondarenko/service/ProjectsService.java
+++ src/main/java/com/anatoly/bondarenko/service/ProjectsService.java
@@ -1,6 +1,43 @@
 package com.anatoly.bondarenko.service;
 
+import com.anatoly.bondarenko.DAO.ProjectsDAO;
+import com.anatoly.bondarenko.domain.Projects;
+
+import java.util.List;
+
 public class ProjectsService {
+
+    private final ProjectsDAO projectsDAO;
+
+
+    public ProjectsService(ProjectsDAO projectsDAO) {
+        this.projectsDAO = projectsDAO;
+    }
+
+
+    public List<Projects> getAllProjects() {
+
+        return projectsDAO.getAllEntities();
+    }
+
+
+    public void deleteProject(Long id) {
+
+        projectsDAO.deleteEntity(id);
+    }
+
+
+    public void addNewProject(Projects project) {
+
+        projectsDAO.addNewEntity(project);
+    }
+
+
+    public void updateProject(Long id, Projects project) {
+
+        projectsDAO.updateEntity(id, project);
+    }
+
 
 
 }
--- src/main/java/com/anatoly/bondarenko/service/SkillsService.java
+++ src/main/java/com/anatoly/bondarenko/service/SkillsService.java
@@ -1,6 +1,42 @@
 package com.anatoly.bondarenko.service;
 
+import com.anatoly.bondarenko.DAO.SkillsDAO;
+import com.anatoly.bondarenko.domain.Skills;
+
+import java.util.List;
+
 public class SkillsService {
+
+    private final SkillsDAO skillsDAO;
+
+
+    public SkillsService(SkillsDAO skillsDAO) {
+        this.skillsDAO = skillsDAO;
+    }
+
+
+    public List<Skills> getAllSkills() {
+
+        return skillsDAO.getAllEntities();
+    }
+
+
+    public void deleteSkill(Long id) {
+
+        skillsDAO.deleteEntity(id);
+    }
+
+
+    public void addNewSkill(Skills skill) {
+
+        skillsDAO.addNewEntity(skill);
+    }
+
+
+    public void updateSkill(Long id, Skills skill) {
+
+        skillsDAO.updateEntity(id, skill);
+    }
 
 
 }
